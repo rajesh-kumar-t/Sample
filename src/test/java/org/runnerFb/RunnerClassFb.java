9e50ed75c99c1c04a503baa1b7105c09196d3f9c
rajesh-kumar-t <rajeshrk1388@gmail.com>
2022-02-16 13:13:48 +0530

    Nothing added

--- src/test/java/org/runnerFb/RunnerClassFb.java
+++ src/test/java/org/runnerFb/RunnerClassFb.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources", glue = "org.stepDefinitionFb", dryRun = true, strict = true)
+@CucumberOptions(features = "src\\test\\resources", glue = "org.stepDefinitionFb", dryRun = false)
 
 public class RunnerClassFb {
 
